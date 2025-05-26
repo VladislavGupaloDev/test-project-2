c7609f3bde45e7adaddfcb4857dd348e755ec6fa
Vladislav Gupalo <parallaxtrashbox@gmail.com>
2025-05-26 23:06:11 +0500

    fix: delete strict mode

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,6 +1,5 @@
 import { App } from './App'
 import { StoreProvider } from './providers/StoreProvider/StoreProvider'
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 const element = document.getElementById('root')!
@@ -8,9 +7,7 @@ const element = document.getElementById('root')!
 const root = createRoot(element)
 
 root.render(
-  <StrictMode>
-    <StoreProvider>
-      <App />
-    </StoreProvider>
-  </StrictMode>
+  <StoreProvider>
+    <App />
+  </StoreProvider>
 )
